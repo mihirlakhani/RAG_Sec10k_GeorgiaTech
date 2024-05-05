bf4de4221b7a7d5b3eb2f21fc980b4987e6dafd3
MIHIR LAKHANI <90389993+mihirlakhani@users.noreply.github.com>
2024-05-05 18:49:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, jsonify
+from sec_edgar_downloader import Downloader
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.palm import PaLM
+
+app = Flask(__name__)
+
+# Configure the LLAMA settings
+Settings.embed_model = HuggingFaceEmbedding(
+    model_name="BAAI/bge-small-en-v1.5",
+    device="cpu"
+)
+Settings.llm = PaLM(api_key="your_api_key")
+
+# Load documents and create the index for LLAMA
+documents_llama = SimpleDirectoryReader(r"C:/Users/DELL/Desktop/sec10k-RAG_Pipeline-master/data").load_data()
+index_llama = VectorStoreIndex.from_documents(documents_llama)
+query_engine_llama = index_llama.as_query_engine()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    ticker = request.form['ticker']
+    limit = request.form.get('limit')
+
+    # Check if limit is not empty and convert to int
+    if limit and limit.strip():  # Check if limit is not empty or whitespace
+        limit = int(limit)
+    else:
+        limit = None  # Set limit to None if it's empty
+
+    dl = Downloader("MyCompanyName", "my.email@domain.com")
+    dl.get(form="10-K", ticker_or_cik=ticker, limit=limit, download_details=True)
+    
+    return jsonify({"success": True})
+
+@app.route('/query', methods=['POST'])
+def query():
+    user_query = request.form['query']
+    response = query_engine_llama.query(user_query)
+    response_text = response.response
+    
+    return jsonify({"response": response_text})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ data_extraction-cleansing.py
@@ -0,0 +1,35 @@
+import requests
+
+
+# API endpoint
+endpoint = 'https://api.sec-api.io/extractor'
+
+# Example parameters
+url = 'https://www.sec.gov/ix?doc=/Archives/edgar/data/789019/000095017023035122/msft-20230630.htm'
+item = '7'
+token = '2b14839b47fcd6dd9e915831a791a681e2e2fb1ed5e0e600461194d33c6b571c'
+item_type = 'html'
+
+# Construct parameters
+params = {
+    'url': url,
+    'item': item,
+    'type': item_type,
+    'token': token
+}
+
+try:
+    # Make GET request to the API
+    response = requests.get(endpoint, params=params)
+    response.raise_for_status()  # Raise exception for HTTP errors
+
+    # Save extracted content to an HTML file
+    with open('app8.html', 'w', encoding='utf-8') as f:
+        f.write(response.text)
+    print("Extracted content saved as 'app8.html'")
+
+    
+    
+
+except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")

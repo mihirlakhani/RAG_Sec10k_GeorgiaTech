69f72cdef2ba74e65b33e177e71f51668a8b7ea9
MIHIR LAKHANI <90389993+mihirlakhani@users.noreply.github.com>
2024-05-05 18:54:04 +0530

    Data cleansing and extraction

--- data_extraction-cleansing.py
+++ data_extraction-cleansing.py
@@ -1,10 +1,8 @@
 import requests
-
-
 # API endpoint
 endpoint = 'https://api.sec-api.io/extractor'
 
-# Example parameters
+# parameters
 url = 'https://www.sec.gov/ix?doc=/Archives/edgar/data/789019/000095017023035122/msft-20230630.htm'
 item = '7'
 token = '2b14839b47fcd6dd9e915831a791a681e2e2fb1ed5e0e600461194d33c6b571c'
